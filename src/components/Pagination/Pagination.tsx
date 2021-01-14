5af43787144c3b5482fab402d1f8dfc2f72cc597
Calvin Job Puram <puram.calvin@gmail.com>
2021-01-15 00:53:45 +0100

    Update Pagination.tsx

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,27 +1,30 @@
 import React, { FC } from "react";
-import Pagination from "react-js-pagination";
 
 type IPaginate = {
   postPerPage: number;
   totalCards: number;
-  activePage: number;
   paginate(pageNumber: number): void;
 };
 
 const PaginationPage: FC<IPaginate> = (props) => {
-  const { totalCards, postPerPage, paginate, activePage } = props;
+  const { totalCards, postPerPage, paginate } = props;
+  const pageNumbers = [];
+
+  for (i = 1; i <= Math.ceil(totalCards / postPerPage); i++) {
+    pageNumbers.push(i);
+  }
 
   return (
     <div className="pagination pagination-lg">
-      <Pagination
-        activePage={activePage}
-        itemsCountPerPage={postPerPage}
-        totalItemsCount={totalCards}
-        pageRangeDisplayed={5}
-        onChange={paginate}
-        itemClass="page-item"
-        linkClass="page-link"
-      />
+      {pageNumbers.map((page: number) => {
+        return (
+          <li class="page-item">
+            <a onClick={() => paginate(number)} class="page-link" href="!#">
+              {page}
+            </a>
+          </li>
+        );
+      })}
     </div>
   );
 };
