a25246ed3dea6c04bf336307fd1431a2bf8c1623
Calvin Job Puram <puram.calvin@gmail.com>
2021-01-15 01:02:40 +0100

    Update Pagination.tsx

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -8,9 +8,9 @@ type IPaginate = {
 
 const PaginationPage: FC<IPaginate> = (props) => {
   const { totalCards, postPerPage, paginate } = props;
-  const pageNumbers:any = [];
+  const pageNumbers:[number] = [];
 
-  for (i = 1; i <= Math.ceil(totalCards / postPerPage); i++) {
+  for (let i = 1; i <= Math.ceil(totalCards / postPerPage); i++) {
     pageNumbers.push(i);
   }
 
